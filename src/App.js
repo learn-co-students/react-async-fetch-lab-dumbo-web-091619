d51c84d5aab3d404d7a4f2ca10a62fb50c4e03cb
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-12-06 20:24:37 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React from 'react'
+
+class App extends React.Component{
+    state = {
+        spacePpl: []
+    }
+ 
+        async componentDidMount(){
+            let rawSpacePpl = await fetch('http://api.open-notify.org/astros.json')
+            let spacePpl = await rawSpacePpl.json()
+            this.setState({
+                spacePpl
+            })
+        }
+
+        render(){
+
+        return(
+            <div>
+              { this.state.spacePpl.map(ppl => ppl.name)}
+            </div>
+        )
+    }
+
+}
+
+export default App
